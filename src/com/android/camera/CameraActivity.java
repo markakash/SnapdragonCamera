c1a423d6e1a13e1fadb31f19dd52dc8c69dacc62
cretin45 <cretin45@gmail.com>
2015-09-29 16:29:29 -0700

    SnapdragonCamera: Use Glide for async bitmap loading
    This is what Camera2 code uses.
    
    CameraNext: Fix rotation not updated
    
    The thumbnail generated by Glide is not updated
    when user rotate the image manually.
    This is because exif is rewrite using nio mmap,
    however there is no nio unmap available in java.
    Even though the file channel is closed, the rotation
    modification is still in buffer, when Glide tries
    to dtermine the orientation of the image, it still
    reads the old data.
    
    Use force to ensure data is sync to disk and buffer flushed.
    
    HAM-1389
    
    Fix memory leaks
    
    	Two leaks were occuring. One was an unused Parameters
    	in AndroidCameraManagerImpl that was holding onto excess
    	PhotoModules. The second was Glide images for video previews that were never being cleared on recycle.
    
    issue-id: CYNGNOS-2731
    
    CameraNext: don't call Glide to load image after Camera exits
    
    There is a race condition where the filmstrip can still be trying
    to load a new image just as the camera exit, and Glide gets
    upset if it is handed a destroyed activity context to work with.
    So just check for that condition and go home.
    
    Change-Id: I9e76ab40d23213c7a74c26779e345207d5fd7bae
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: Akash Srivastava <akashniki@gmail.com>

--- src/com/android/camera/CameraActivity.java
+++ src/com/android/camera/CameraActivity.java
@@ -120,6 +120,10 @@ import com.android.camera.util.PhotoSphereHelper;
 import com.android.camera.util.PhotoSphereHelper.PanoramaViewHelper;
 import com.android.camera.util.UsageStatistics;
 import org.codeaurora.snapcam.R;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.GlideBuilder;
+import com.bumptech.glide.MemoryCategory;
+import com.bumptech.glide.load.engine.executor.FifoPriorityThreadPoolExecutor;
 
 import java.io.File;
 import java.io.IOException;
@@ -1615,8 +1619,7 @@ public class CameraActivity extends Activity
                 FilmStripView.ImageData.SIZE_FULL);
         // Put a CameraPreviewData at the first position.
         mWrappedDataAdapter = new FixedFirstDataAdapter(
-                new CameraDataAdapter(new ColorDrawable(
-                        getResources().getColor(R.color.photo_placeholder))),
+                new CameraDataAdapter(R.color.photo_placeholder),
                 mCameraPreviewData);
 
         mFilmStripView.setViewGap(
@@ -1684,9 +1687,7 @@ public class CameraActivity extends Activity
         display.getSize(size);
         int width = size.x;
         int height = size.y;
-
         int lower = Math.min(width, height);
-
         int offset = lower * 7 / 100;
         SETTING_LIST_WIDTH_1 = lower / 2 + offset;
         SETTING_LIST_WIDTH_2 = lower / 2 - offset;
@@ -1700,6 +1701,13 @@ public class CameraActivity extends Activity
 
         if (PersistUtil.isTraceEnable())
             Trace.endSection();
+        }
+
+        if (!Glide.isSetup()) {
+            Glide.setup(new GlideBuilder(getApplicationContext())
+                    .setResizeService(new FifoPriorityThreadPoolExecutor(2)));
+            Glide.get(getApplicationContext()).setMemoryCategory(MemoryCategory.HIGH);
+        }
     }
 
     private void setRotationAnimation() {
