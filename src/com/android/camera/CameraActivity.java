aa6214e14fdbcd65280d0a594ba2a9d1888a5021
Sultanxda <sultanxda@gmail.com>
2016-03-20 14:18:37 -0700

    ParanoidCamera: Always enable developer menu

--- src/com/android/camera/CameraActivity.java
+++ src/com/android/camera/CameraActivity.java
@@ -393,7 +393,8 @@ public class CameraActivity extends Activity
     }
 
     public boolean isDeveloperMenuEnabled() {
-        return mDeveloperMenuEnabled;
+        //return mDeveloperMenuEnabled;
+        return true;
     }
 
     public void enableDeveloperMenu() {
