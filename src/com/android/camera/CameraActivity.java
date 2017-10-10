881ed32fbb24b6305dd063b94d01f26d42e16ef1
Park Ju Hyung <qkrwngud825@gmail.com>
2017-10-10 12:23:05 +0900

    ParanoidCamera: ensure UI is drawn in portrait
    When onCreate() gets called during Android rotation,
    the orientation state might differ between when onCreate() gets called
    and when it returns, effectively corrupting the UI due to improper calculation.
    
    To fix this, ensure UI is drawn in portrait from onCreate().
    onCreate() will be called after the rotation is done
    with the new AndroidManifest.xml change.
    
    mRedrawing doesn't have to be wrapped within mutex
    since Android activity cycle makes it exclusive anyways.
    
    Change-Id: I669dc7faec891bab86ac0131c0926f9ae763cbed
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- src/com/android/camera/CameraActivity.java
+++ src/com/android/camera/CameraActivity.java
@@ -79,6 +79,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.OrientationEventListener;
+import android.view.Surface;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
@@ -242,6 +243,7 @@ public class CameraActivity extends Activity
     private FrameLayout mPreviewContentLayout;
     private boolean mPaused = true;
     private boolean mForceReleaseCamera = false;
+    private boolean mRedrawing = false;
 
     private Uri[] mNfcPushUris = new Uri[1];
 
@@ -1498,6 +1500,18 @@ public class CameraActivity extends Activity
             e.printStackTrace();
         }
 
+        // Do not draw UI until rotation animation has finished
+        final int orientation = getScreenOrientation();
+        if (orientation != ActivityInfo.SCREEN_ORIENTATION_PORTRAIT &&
+            orientation != ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT) {
+            mRedrawing = true;
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            return;
+        }
+        if (mRedrawing) {
+            mRedrawing = false;
+        }
+
         final boolean useHal3 = getResources().getBoolean(R.bool.config_use_hal3);
         CameraManagerFactory.getAndroidCameraManager().setHal3(useHal3);
 
@@ -1701,6 +1715,29 @@ public class CameraActivity extends Activity
         }
     }
 
+    private int getScreenOrientation() {
+        final int rotation = getWindowManager().getDefaultDisplay().getRotation();
+        final int orientation = getResources().getConfiguration().orientation;
+
+        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            if (rotation == Surface.ROTATION_0 || rotation == Surface.ROTATION_270) {
+                return ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
+            } else {
+                return ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT;
+            }
+        }
+
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            if (rotation == Surface.ROTATION_0 || rotation == Surface.ROTATION_90) {
+                return ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
+            } else {
+                return ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE;
+            }
+        }
+
+        return ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED;
+    }
+
     private void setRotationAnimation() {
         int rotationAnimation = WindowManager.LayoutParams.ROTATION_ANIMATION_ROTATE;
         rotationAnimation = WindowManager.LayoutParams.ROTATION_ANIMATION_CROSSFADE;
@@ -1752,7 +1789,7 @@ public class CameraActivity extends Activity
 
     @Override
     public void onPause() {
-        if (mSecureCamera && !hasCriticalPermissions()) {
+        if (mSecureCamera && !hasCriticalPermissions() || mRedrawing) {
             super.onPause();
             return;
         }
@@ -1794,7 +1831,7 @@ public class CameraActivity extends Activity
     public void onWindowFocusChanged(boolean focus) {
         // Hide action bar first since we are in full screen mode first, and
         // switch the system UI to lights-out mode.
-        if (focus) this.setSystemBarsVisibility(false);
+        if (focus && !mRedrawing) this.setSystemBarsVisibility(false);
     }
 
     /**
@@ -1842,6 +1879,10 @@ public class CameraActivity extends Activity
 
     @Override
     public void onResume() {
+        if (mRedrawing) {
+            super.onResume();
+            return;
+        }
         if (mSecureCamera && !hasCriticalPermissions()) {
             super.onResume();
             showOpenCameraErrorDialog();
@@ -1913,20 +1954,24 @@ public class CameraActivity extends Activity
     @Override
     public void onStart() {
         super.onStart();
-        if (mSecureCamera && !hasCriticalPermissions()) {
+        if (mSecureCamera && !hasCriticalPermissions() || mRedrawing) {
             return;
         }
         bindMediaSaveService();
-        mPanoramaViewHelper.onStart();
+        if (mPanoramaViewHelper != null) {
+            mPanoramaViewHelper.onStart();
+        }
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        if (mSecureCamera && !hasCriticalPermissions()) {
+        if (mSecureCamera && !hasCriticalPermissions() || mRedrawing) {
             return;
         }
-        mPanoramaViewHelper.onStop();
+        if (mPanoramaViewHelper != null) {
+            mPanoramaViewHelper.onStop();
+        }
         unbindMediaSaveService();
     }
 
